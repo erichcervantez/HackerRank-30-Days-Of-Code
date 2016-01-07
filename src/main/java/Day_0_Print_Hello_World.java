bb761f6bc77ad85cc8fa4557def7d481f0d0ea50
Erich Cervantez <git@noofusion.com>
2016-01-06 20:10:57 -0800

    Adding initial challenges

---
+++ src/main/java/Day_0_Print_Hello_World.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Day_0_Print_Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World.");
+        System.out.println("Welcome to 30 Days of Code.");
+    }
+}
---
+++ src/main/java/Day_1_Data_Types.java
@@ -0,0 +1,20 @@
+package main.java;
+
+/**
+ * https://www.hackerrank.com/contests/30-days-of-code/challenges/day-1-data-types
+ */
+public class Day_1_Data_Types
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Primitive : double");
+        System.out.println("Primitive : char");
+        System.out.println("Primitive : boolean");
+        System.out.println("Primitive : int");
+        System.out.println("Referenced : String");
+        System.out.println("Primitive : boolean");
+        System.out.println("Primitive : double");
+        System.out.println("Primitive : char");
+        System.out.println("Referenced : String");
+    }
+}
